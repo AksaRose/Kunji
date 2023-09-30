aacc6ba5b99ec83c73c552a064353293cfab8481
vinsu353 <125719348+vinsu353@users.noreply.github.com>
2023-09-30 15:23:19 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,10 +1,10 @@
 async function getLink(){
-    var link = document.getElementById("link").value;
+    var link = document.getElementById("inputurl").value;
     const api_url=`https://api.shrtco.de/v2/shorten?url=${link}`
     const response=await fetch(api_url)
     const data=await response.json()
     res=data.result.full_short_link2
-    document.getElementById('mini-link').value=res
+    document.getElementById('outputurl').value=res
     console.log(res)
 }
 document.getElementById('gen').addEventListener('click',getLink)
