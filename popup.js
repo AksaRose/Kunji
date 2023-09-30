a8fcc4512a3f59e99c94810be0153cc01185d66b
vinsu353 <125719348+vinsu353@users.noreply.github.com>
2023-09-30 14:26:56 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 async function getLink(){
     var link = document.getElementById("link").value;
-    const api_url=`https://yoursite/yourls-api.php?timestamp=$timestamp&signature=$signature&action=1002a612b4${link}`
+    const api_url=`https://api.shrtco.de/v2/shorten?url=${link}`
     const response=await fetch(api_url)
     const data=await response.json()
     res=data.result.full_short_link2
