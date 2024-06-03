b09ae4516bd02a5a530043ea66c5d76b5b830789
AksaRose <aksarose3@gmail.com>
2024-06-03 13:01:38 +0530

    build : rewrote with new API

--- popup.js
+++ popup.js
@@ -1,11 +1,45 @@
-async function getLink(){
-    var link = document.getElementById("inputurl").value;
-    const api_url=`https://api.shrtco.de/v2/shorten?url=${link}`
-    const response=await fetch(api_url)
-    const data=await response.json()
-    console.log(data);
-    res=data.result.full_short_link2
-    document.getElementById('outputurl').value=res
-    console.log(res)
-}
-document.getElementById('gen').addEventListener('click',getLink)
+document.addEventListener('DOMContentLoaded', function() {
+    
+
+    const button = document.getElementById('gen');
+    const outputurl = document.getElementById('outputurl');
+
+    button.addEventListener('click', function() {
+        const inputurl = document.getElementById('inputurl').value;
+        console.log(inputurl)
+        if (inputurl.trim() === '') {
+            alert('Oopsy!!, Please enter URL.');
+        }
+        else {
+            const requestBody = {
+                url: inputurl
+            };
+            const options = {
+                method: 'POST',
+                headers: {
+                    Authorization: 'Bearer mA4qhb4hTlaLGOfoUuQJodhr',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(requestBody)
+            };
+          
+            fetch('https://api.dub.co/links?workspaceId=ws_clwvjjc4w0000es8tijnh0v5q', options)
+            .then(response => response.json())
+            .then(response => {
+                if (response.shortLink){
+                    outputurl.value = response.shortLink;
+                }
+                else{
+                    alert("Failed to shorten URL");
+                }
+            })
+            .catch(err => {
+                console.error(err);
+                alert('An error occurred while shortening the URL');
+            });
+        }
+    });
+
+                
+            
+});
