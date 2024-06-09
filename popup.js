b70206654a2b14805983ec14ffac507301d87bba
Aksa-Rose <aksarose3@gmail.com>
2024-06-09 23:08:47 +0530

    feat: added qrCode

--- popup.js
+++ popup.js
@@ -4,6 +4,15 @@ document.addEventListener('DOMContentLoaded', function() {
     const button = document.getElementById('gen');
     const outputurl = document.getElementById('outputurl');
     const code = document.getElementById('code');
+    const imagecont = document.getElementById('image-container');
+
+    const image = document.createElement('img');
+    image.src = "kunjiicon.png";
+    image.width = '140';
+    image.height = '159';
+    image.alt = 'Kunji Icon';
+
+    imagecont.appendChild(image);
 
     button.addEventListener('click', function() {
         const inputurl = document.getElementById('inputurl').value;
@@ -18,18 +27,19 @@ document.addEventListener('DOMContentLoaded', function() {
             const options = {
                 method: 'POST',
                 headers: {
-                    Authorization: 'Bearer mA4qhb4hTlaLGOfoUuQJodhr',
+                    Authorization: 'Bearer <Your-Token>',
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify(requestBody)
             };
           
-            fetch('https://api.dub.co/links?workspaceId=ws_clwvjjc4w0000es8tijnh0v5q', options)
+            fetch('https://api.dub.co/links?workspaceId=<Your-Workspace-ID>', options)
             .then(response => response.json())
             .then(response => {
                 if (response.shortLink && response.qrCode){
                     outputurl.value = response.shortLink;
                     code.value = response.qrCode;
+                    
                 }
                 else{
                     alert("Failed to shorten URL");
